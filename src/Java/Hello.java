b15a8e21532e152f58591a19d8283dfd6309f866
peng <1511784821@qq.com>
2021-11-27 19:25:08 +0800

    reset

--- src/Java/Hello.java
+++ src/Java/Hello.java
@@ -1,8 +1,7 @@
 package Java;
 
 public class Hello {
-    //更新
-    public static void main(String args[]) {
+    public static void main(String args[]){
         System.out.println("Helloworld");
     }
 }
